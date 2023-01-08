f20ca328b077d1edc3d7babeeffa4b936612fe8d
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-01-08 19:44:41 +0100

    hecho hasta 5B

--- TEMA6_1EV/src/ejerciciosB/ej5.java
+++ TEMA6_1EV/src/ejerciciosB/ej5.java
@@ -1,5 +1,6 @@
 package ejerciciosB;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,36 +10,38 @@ import java.util.Scanner;
 
 public class ej5 {
     
-    // Crea un programa que muestre por pantalla cuantas vocales de cada tipo 
-    // hay (cuantas ‘a’, cuantas ‘e’, etc.) en una frase introducida por teclado. 
-    // No se debe diferenciar entre mayúsculas y minúsculas. Por ejemplo dada 
-    // la frase “Mi mama me mima” dirá que hay:
-    // NºdeA's:3 NºdeE's:1 NºdeI's:2 NºdeO's:0 NºdeU's:0
+    // Realiza un programa que lea una frase por teclado e indique si la frase 
+    // es un palíndromo o no (ignorando espacios y sin diferenciar entre 
+    // mayúsculas y minúsculas). Supondremos que el usuario solo introducirá 
+    // letras y espacios (ni comas, ni puntos, ni acentos, etc.). Un palíndromo 
+    // es un texto que se lee igual de izquierda a derecha que de derecha a 
+    // izquierda. Ej: "Amo la pacífica paloma"
     
     public static void main(String[] args) {
     
         Scanner reader = new Scanner(System.in); 
-        String frase="",fraseLow;
+        String frase="",fraseLow,fraseTrim;
         char[] arrayFrase;
-        int[] countVocales = {0, 0, 0, 0, 0};
-        char[] vocales = {'a','e','i','o','u'};
         
         System.out.println("Introduce una frase: ");
         frase = reader.nextLine();
         
-        arrayFrase = frase.toCharArray();
         fraseLow = frase.toLowerCase();
+        fraseTrim = fraseLow.replace(" ","");
+        arrayFrase = fraseTrim.toCharArray();
         
-        for (int i = 0; i < arrayFrase.length; i++) { 
-           char letra = fraseLow.charAt(i);
-            
-           for( int j= 0; j < vocales.length; j++){
-              if(letra == vocales[j]){
-                  countVocales[j]++;
-              } 
-           }
+        
+        char[]revArrayFrase = new char [arrayFrase.length];
+        
+        for (int i = 0; i < arrayFrase.length; i++){
+            revArrayFrase[(arrayFrase.length - 1 -i)] = arrayFrase[i];
+        }
+        
+        if(Arrays.equals(revArrayFrase, arrayFrase)){
+            System.out.println("Es palíndromo.");
+        }else{
+            System.out.println("No es palíndromo.");
         }
 
-        System.out.println(" Nº de A's: "+countVocales[0]+"\n Nº de E's: "+countVocales[1]+"\n Nº de I's: "+countVocales[2]+"\n Nº de O's: "+countVocales[3]+"\n Nº de U's: "+countVocales[4]);
     }
 }
