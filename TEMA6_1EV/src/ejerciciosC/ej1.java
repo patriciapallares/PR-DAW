84ffcf4b97f36e0a6ab080fa88b07aa43647678f
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-01-08 19:46:38 +0100

    creado paquete ejerciciosC

---
+++ TEMA6_1EV/src/ejerciciosC/ej1.java
@@ -0,0 +1,33 @@
+package ejerciciosC;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author patriciapallares
+ */
+
+public class ej1 {
+    
+    // PENDIENTE
+    
+    
+    public static void main(String[] args) {
+    /*
+        Scanner reader = new Scanner(System.in);  
+        char[] aCaracteres;
+        int longitud;
+        String cadena = "", posiciones="";
+        String palabraS = "";
+        
+        System.out.println("Introduce una frase: ");
+        cadena = reader.nextLine();
+        
+        
+        aCaracteres = cadena.toCharArray();
+    */
+    
+    }
+}
