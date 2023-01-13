c6c7dcfebf17112e594ed924edf6abb2be3d2a36
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-01-13 13:30:57 +0100

    realizado hasta 3 de tema 7

--- TEMA7_1EV/src/ejercicios/ej1.java
+++ TEMA7_1EV/src/ejercicios/ej1.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package ejercicios;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,4 +8,30 @@ package ejercicios;
  */
 public class ej1 {
 
+    // Escribe un programa que pida dos números reales por teclado y muestre por
+    // pantalla el resultado de multiplicarlos. Implementa y utiliza la función:
+    // double multiplica(double a, double b) // Devuelve la multiplicación de dos números
+    
+    public static void main(String[] args){
+                         
+        Scanner reader = new Scanner(System.in);  
+        double N,M,resultado;
+        
+        System.out.println("Introduce un número: ");
+        N = reader.nextDouble();
+        
+        System.out.println("Introduce un otro número: ");
+        M = reader.nextDouble();
+        
+        resultado = multiplica(N,M);
+        
+        System.out.println("El resultado es: "+resultado);
+
+    }
+    
+    public static double multiplica(double a, double b){
+        double resp = a * b;
+        
+        return resp;
+    }
 }
