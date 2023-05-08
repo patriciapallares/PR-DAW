338f9a848c3491910aa9245a0319daf189727de9
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-05-08 08:09:20 +0200

    cambios 3B

--- TEMA11_3EV/src/tema11_3ev/B_ej3_11.java
+++ TEMA11_3EV/src/tema11_3ev/B_ej3_11.java
@@ -1,22 +1,52 @@
 package tema11_3ev;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class B_ej3_11 {
 
     public static void main(String[] args) {
 
-        File usa_personas = new File("/Users/patriciapallares/Downloads/Documentos/usa_personas.txt");
-        
-        
-        // programa que 
-        
-        // pide nombre del archivo por teclado
-        // A para lectura y B para escritura
-        
-        // Lee el contenido de A y lo escribe ordenado alfabéticamente en B
-        
-        
-        
+        try {
+            File usa_personas = new File("/Users/patriciapallares/Downloads/Documentos/usa_personas.txt");
+            // File usa_personas_sorted = new File("/Users/patriciapallares/Downloads/Documentos/usa_personas_sorted.txt");
+            Scanner reader = new Scanner(System.in);
+
+            String lectura = "", escritura = "";
+            // programa que 
+
+            // pide nombre del archivo por teclado
+            // A para lectura y B para escritura
+            // Lee el contenido de A y lo escribe ordenado alfabéticamente en B
+            System.out.println("Introduce el nombre del archivo para lectura: ");
+            lectura = reader.nextLine();
+
+            System.out.println("Introduce el nombre del archivo para escritura: ");
+            escritura = reader.nextLine();
+            String rutaPadre = "/Users/patriciapallares/Downloads/Documentos/";
+            String rutaDestino = rutaPadre.concat(escritura).concat(".txt");
+
+            System.out.println("Ruta Destino = " + rutaDestino);
+
+
+            
+            File f = new File("Enteros.txt");
+            FileWriter fw = new FileWriter(f);
+            int valor = 1;
+            for (int i = 1; i <= 20; i++) {
+                fw.write("" + valor); // escribimos valor
+                fw.write(" "); // escribimos espacio en blanco 
+                valor = valor * 2; // calculamos próximo valor
+            }
+            fw.write("\n"); // escribimos nueva línea 
+            fw.close(); // cerramos el FileWriter 
+            System.out.println("Fichero escrito correctamente");
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+             
+        }
     }
-}
